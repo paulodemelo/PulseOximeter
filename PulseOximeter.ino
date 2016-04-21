0590e08f8a2d990837a67721a41359787b16e2c5
Paulo <ethicz@gmail.com>
2016-04-21 15:28:49 -0400

    Calibration added

--- PulseOximeter.ino
+++ PulseOximeter.ino
@@ -2,14 +2,14 @@
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2); 
 int pulseSpike = 0;
-int pulseThreshold = 2.0; //to be determined by oscilliscope
+int pulseThreshold = 1.5; //to be determined by oscilliscope
 int ledPin = 0;
 int irPin = 1;
 int switchPin = 2;
 int count = 0;
 float recordTime[200] = {0}; 
-float BPM = 0;
-unsigned long previousMillis = 0;        
+int BPM = 0;
+unsigned long previousMillis = 0;
 const long interval = 1000;    
 boolean above = false, below = false;       
 
@@ -35,24 +35,18 @@ void loop() {
 	// Start recording
 	else if ((startSwitch >= 3) && (count < 61)) {
 		// timer for recording, stops at 60 seconds  
-		while (count < 61) {   
+	    while (count < 61) {   
 			float startSwitch = analogRead(0) * (5.0/1023.0);
-			timer(); // Start timer  
 			recordData(); // Start recording data
 			// Checking for pulse spikes
-			if (pulseSignal > pulseThreshold) {
-				above = true;
-				float pulseSignal = analogRead(ledPin) * (5.0/1023.0);
-				if (pulseSignal < pulseThreshold) {
-					below = true;
-				}
-			}
-			// Display
-			lcd.setCursor(0, 0);
-			lcd.print("Recording..     ");  
-			lcd.setCursor(14, 0);
-			lcd.print(count);
-			checkPulse(above, below);
+            if (pulseSignal < pulseThreshold) {
+                float pulseSignal = analogRead(A0) * (5.0/1023.0);
+                above = true;
+                if (pulseSignal > pulseThreshold) {
+                    below = true;
+                }
+            }
+            checkPulse(above, below);
 			// Stops recording
 			if (startSwitch < 3) { 
 				break;
@@ -71,65 +65,81 @@ void loop() {
 
 // Function to calculate pulse rate
 float calcPulse(int x, float arr[]) {
-	float avg = 1;
-	float sum = 0;
-	if (x > 0) {
-		for (int i = 1; i < x; i++) {
-			sum += arr[i] - arr[i-1];
-		}
-	}
-	avg = (sum / x);
-	return 60/avg; 
+    float avg = 1;
+    float sum = 0;
+    float last = 0;
+    if (x > 1) {
+        for (int i = 1; i < x; i++) {
+            sum += arr[i] - arr[i-1];
+        }
+    }
+    last = arr[x] - arr[x-1]; // just displays last pulse
+    avg=sum/x;
+    return 60/avg; 
 }
 
-//Function to check if pulse spike accured
-void checkPulse(boolean a, boolean b) {
-	if (a && b){
-		above = false;
-		below = false;  
-		recordTime[pulseSpike] = float(millis() / 1000.00);
-		BPM = calcPulse(pulseSpike, recordTime);       
-		pulseSpike++;
-		lcd.setCursor(0, 1);
-		lcd.print("Pulse:          ");
-		lcd.setCursor(7, 1);
-		lcd.print(BPM);
-		if (pulseSpike == 200) { // Just in case array gets filled
-			pulseSpike = 0;  
-			float recordTime[200] = {0} 
-		}
-	}
+// Function to check if pulse spike accured
+int checkPulse(boolean a, boolean b) {
+    if (a && b){
+        above = false;
+        below = false;  
+        recordTime[pulseSpike] = float(millis() / 1000.00);
+        BPM = calcPulse(pulseSpike, recordTime);       
+        pulseSpike++;
+        lcd.setCursor(0, 1);
+        lcd.print("Pulse:          ");
+        lcd.setCursor(7, 1);
+        lcd.print(BPM);
+        if (pulseSpike == 199) { // Just in case array gets filled
+            float recordTime[200] = {0};
+            pulseSpike = 0;
+        }
+    }
 }
 
 // Function to record and store data file
 void recordData() {
-	String dataString = "";
-	// Record Time,Voltage; new line
-	for (int analogPin = 0; analogPin < 2; analogPin++) { 
-		float sensor = analogRead(0) * (5.0/1023.0); //change back to analogPin *******
-		dataString += (millis() / 1000.00);
-		dataString += ",";
-		dataString += String(sensor);
-		dataString += ";"; 
-		if (analogPin == 0) {
-			dataString += "\n";
-		}
-	}
-	Serial.println(dataString);
+    // Calibration test.. wait for at least 5 spikes to start recording
+    if (pulseSpike > 4) { 
+    	String dataString = "";
+    	// Record Time,Voltage; new line
+    	for (int analogPin = 0; analogPin < 2; analogPin++) { 
+    		float sensor = analogRead(0) * (5.0/1023.0); //change back to analogPin *******
+    		dataString += (millis() / 1000.00);
+    		dataString += ",";
+    		dataString += String(sensor);
+    		if (analogPin == 0) {
+    			dataString += ",";
+    		}
+    	}
+    	Serial.println(dataString);
+        timer();
+        lcd.setCursor(0, 0);
+        lcd.print("Recording...    ");  
+        lcd.setCursor(14, 0);
+        lcd.print(count);
+    }
+    else
+    {
+        float sensor = analogRead(0) * (5.0/1023.0);
+        Serial.println(sensor);
+        lcd.setCursor(0, 0);
+        lcd.print("Calibrating...  ");   
+    }
 
-	File dataFile = SD.open("datalog.txt", FILE_WRITE);
-	
-	if (dataFile) {
-		dataFile.println(dataString);
-		dataFile.close();
-	}
-
-	else {
-		lcd.setCursor(0, 0);
-		lcd.print("ERROR Could not "); 
-		lcd.setCursor(0, 1);
-		lcd.print("open file       "); 
-	}
+//	File dataFile = SD.open("datalog.txt", FILE_WRITE);
+//	
+//	if (dataFile) {
+//		dataFile.println(dataString);
+//		dataFile.close();
+//	}
+//
+//	else {
+//		lcd.setCursor(0, 0);
+//		lcd.print("ERROR Could not "); 
+//		lcd.setCursor(0, 1);
+//		lcd.print("open file       "); 
+//	}
 }
 
 // Simple timer function
